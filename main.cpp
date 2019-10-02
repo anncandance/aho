06a08b5a83a6d9437b6e4d234ac4b2d6b288c99e
ann <marsdragoncheck@gmail.com>
2019-10-02 19:56:12 +0300

    min_period

--- main.cpp
+++ main.cpp
@@ -1,25 +1,13 @@
 #include <iostream>
-#include <string>
-#include <map>
-
-#include "aho_corasic_search_funk.h"
-#include "node.h"
+#include"period.h"
 
 using namespace std ;
 
 
 int main() {
-    int n;
-    cout<<"enter a number of words:"<<endl;
-    cin>>n;
-    string word;
-    map<int, string> wordlist = create_wordlist(n);
-    string a;
-    char c;
-    cout<<"enter text and press 0 at the end: "<<endl;
-    cin>>noskipws>>c;
-    getline(cin, a, '0');
-    node* thicket = create_thicket(wordlist);
-    Aho_search(thicket, a);
+    string s;
+    cout<<"enter a string "<<endl;
+    cin>>s;
+    cout<<min_period(s);
     return 0;
 }
